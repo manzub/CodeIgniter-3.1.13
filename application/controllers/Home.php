d0f83d9859ca85046f34aab8426401de1d6ee04a
WizzDom <41767916+manzub@users.noreply.github.com>
2024-08-12 21:30:19 +0100

    added contact us

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,6 +7,8 @@ class Home extends Member_Controller
 	public function __construct()
 	{
 		parent::__construct();
+		$this->load->helper('email_helper');
+
 		$this->load->model('model_surveys');
 		$this->load->model('model_reviews');
 		$this->load->model('model_transcribe');
@@ -81,11 +83,51 @@ class Home extends Member_Controller
 		$this->render_template('pages/home', $this->data);
 	}
 
-	public function contact_us() {
+	public function contact_us()
+	{
+		$this->data['title'] = 'SurveyVine | Contact Us';
 
+		$this->form_validation->set_rules('fname', 'First Name', 'trim|required');
+		$this->form_validation->set_rules('email', 'Email Address', 'trim|required');
+		$this->form_validation->set_rules('category', 'Category', 'required');
+		$this->form_validation->set_rules('message', 'Message', 'trim|required');
+
+		if ($this->form_validation->run() == true) {
+			$lastName = !empty($this->input->post('lname')) ? $this->input->post('lname') : '';
+			$fromName = $this->input->post('fname') . ' ' . $lastName;
+
+			$subject = "A query has been submitted on SurveyVine!";
+
+			$message = "<div style='align-items:left'>
+			<h1>A new query has been submitted on SurveyVine.com</h1>
+			<p>Here are the details.\n</p>
+			<h3>From Name: ".$fromName."</h3>
+			<h3>Email: ".$this->input->post('email')."</h3>
+			<h3>Category: ".$this->input->post('category')."\n\n</h3>
+			<p>Message details below:</p>
+			<p>".$this->input->post('message')."</p>
+			</div>";
+
+			$email_body = html_email_template($message, $subject, null, $fromName);
+
+			$this->email->from('noreply@surveyvine.com');
+			$this->email->to('noreply@surveyvine.com');
+			$this->email->subject($subject);
+			$this->email->message($email_body);
+			$this->email->set_mailtype('html');
+
+			if ($this->email->send()) {
+				$this->session->set_flashdata('alert', array('classname' => 'alert-success', 'title' => 'Query Submitted', 'message' => 'Your query has been submitted'));
+			} else {
+				echo $this->email->print_debugger();
+				$this->session->set_flashdata('alert', array('classname' => 'alert-warning', 'title' => 'An error occurred', 'message' => 'an error occurred and could not complete your request, try again later.'));
+			}
+		}
+
+		$this->render_template('pages/contact_us', $this->data);
 	}
 
-	public function about_us() {
-		
+	public function about_us()
+	{
 	}
 }
---
+++ application/views/pages/contact_us.php
@@ -0,0 +1,78 @@
+<div id="y-col1">
+	<div class="mod view_default">
+		<div class="bd type_fptoday type_fptoday_default">
+
+			<?php echo validation_errors('<div class="alert alert-warning">', '</div>') ?>
+			<?php if ($this->session->flashdata('alert')) { ?>
+				<div class="alert <?php echo $this->session->flashdata('alert')['classname']; ?> alert-dismissible" role="alert">
+					<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+					<strong><?php echo $this->session->flashdata('alert')['title']; ?></strong>
+					<p><?php echo $this->session->flashdata('alert')['message']; ?></p>
+				</div>
+			<?php } ?>
+
+			<div class="row">
+				<div class="col-md-12 mx-auto">
+					<div class="card bg-light">
+						<div class="card-header">
+							<div class="text-center">
+								<h1 style="font-size:20px;font-weight:bold">Leave us a message</h1>
+								<p>Do you have a requests or queries, leave us a message at <a href="mailto://contact@surveyvine.com">contact@surveyvine.com</a></p>
+							</div>
+						</div>
+						<div class="card-body mt-auto mx-auto p-2">
+							<div class="container">
+								<form action="<?php echo base_url('home/contact_us') ?>" role="form" method="post">
+									<div class="row">
+										<div class="col-md-6">
+											<div class="form-group">
+												<label for="">First Name*</label>
+												<input type="text" required class="form-control" name="fname" placeholder="Enter your firstname">
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="form-group">
+												<label for="">Last Name</label>
+												<input type="text" class="form-control" name="lname" placeholder="Enter your lastname">
+											</div>
+										</div>
+									</div>
+
+									<div class="form-group">
+										<label for="">Email address*</label>
+										<input type="email" required class="form-control" name="email" placeholder="enter your email address">
+									</div>
+									<div class="form-group">
+										<label for="">Please select a category*</label>
+										<select id="form_need" name="category" class="form-control" required="required" data-error="Please select a category.">
+											<option value="" selected disabled>--Select Your Issue--</option>
+											<option>Request Invoice for order</option>
+											<option>Request order status</option>
+											<option>Haven't received cashback yet</option>
+											<option>Other</option>
+										</select>
+									</div>
+
+									<div class="row">
+										<div class="col-md-12">
+											<div class="form-group">
+												<label for="form_message">Message *</label>
+												<textarea id="form_message" name="message" class="form-control" placeholder="Write your message here." rows="4" required="required" data-error="Please, leave us a message."></textarea>
+											</div>
+										</div>
+
+										<div class="col-md-12">
+											<input type="submit" class="btn btn-primary btn-send pt-2 btn-block" value="Send Message">
+										</div>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
